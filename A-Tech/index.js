4443706d7755fb6c21be7ce5edbba3a95bca0bfe
AlexandruAlex10 <102253497+AlexandruAlex10@users.noreply.github.com>
2022-10-22 00:36:14 +0300

    Add files via upload

--- A-Tech/index.js
+++ A-Tech/index.js
@@ -6,4 +6,22 @@ $(document).ready(function(){
         items: 1
     });
 
+    // top sale owl carousel
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
+
 });
