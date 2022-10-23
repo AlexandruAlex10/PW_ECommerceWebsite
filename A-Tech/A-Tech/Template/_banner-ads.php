8cf84cd87f2c8f6ca46740da7942af995436189b
AlexandruAlex10 <102253497+AlexandruAlex10@users.noreply.github.com>
2022-10-23 13:13:11 +0300

    Delete A-Tech directory

--- A-Tech/A-Tech/Template/_banner-ads.php
+++
@@ -1,8 +0,0 @@
-<!-- Banner Ads  -->
-<section id="banner_adds">
-    <div class="container py-5 text-center">
-        <img src="./images/Other/bannerad1.jpg" alt="bannerad1" class="img-fluid">
-        <img src="./images/Other/bannerad2.jpg" alt="bannerad2" class="img-fluid">
-    </div>
-</section>
-<!-- !Banner Ads  -->
--- A-Tech/A-Tech/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //require MySQL Connection
-    require('database/DBController.php');
-
-    //Db Controller Object
-    $db = new DBController();
